b30bb47fdf8ffb6b0e9986b6f922ee3e72d528ce
Justin2997 <justin.brlotte797@gmail.com>
2019-07-04 11:15:57 -0400

    Adding User Switcher

--- src/application/Home/TopNav/TopNav.jsx
+++ src/application/Home/TopNav/TopNav.jsx
@@ -3,41 +3,112 @@ import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
+import { connect } from "react-redux";
+import { setUser } from "$store/actions/user";
+
+import { withStyles } from "@material-ui/core/styles";
 import styles from "./TopNav.css";
 
-const useStyles = makeStyles(styles);
+class TopNav extends React.PureComponent {
+  static propTypes = {
+    onDrawerToggle: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  };
 
-const propTypes = {
-  onDrawerToggle: PropTypes.func.isRequired,
-};
+  constructor(props) {
+    super(props);
 
-const TopNav = ({ onDrawerToggle }) => {
-  const classes = useStyles();
+    this.state = {
+      anchorEl: null,
+      open: false,
+    };
 
-  return (
-    <AppBar position="fixed" className={classes.appBar}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          edge="start"
-          onClick={onDrawerToggle}
-          className={classes.menuButton}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6">
-          Magasin UdeS
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
-};
+    this.handleProfileMenuOpen = this.handleProfileMenuOpen.bind(this);
+    this.handleProfileMenuClose = this.handleProfileMenuClose.bind(this);
+  }
 
-TopNav.propTypes = propTypes;
+  handleProfileMenuOpen(event) {
+    this.setState({
+      anchorEl: event.currentTarget,
+      open: true,
+    });
+  }
 
-export default TopNav;
+  handleProfileMenuClose(user) {
+    this.props.dispatch(setUser(user));
+
+    this.setState({
+      anchorEl: null,
+      open: false,
+    });
+  }
+
+  render() {
+    const { classes, onDrawerToggle } = this.props;
+
+    return (
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={onDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            Magasin UdeS
+          </Typography>
+          <IconButton
+            edge="end"
+            aria-label="Account of current user"
+            aria-haspopup="true"
+            onClick={this.handleProfileMenuOpen}
+            color="inherit"
+            className={classes.menuRightIcon}
+          >
+            <AccountCircle />
+          </IconButton>
+          <Typography variant="h6">
+            {this.props.user.name}
+          </Typography>
+          <Menu
+            id="menu-appbar"
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={this.state.open}
+            onClose={this.handleProfileMenuClose}
+          >
+            <MenuItem key="r2d21010" onClick={() => this.handleProfileMenuClose({ name: "R2-D2", cip: "rrdd2222" })}>R2-D2</MenuItem>
+            <MenuItem key="chbc1000" onClick={() => this.handleProfileMenuClose({ name: "Chewbacca", cip: "chbc1000" })}>Chewbacca</MenuItem>
+            <MenuItem key="yoda9999" onClick={() => this.handleProfileMenuClose({ name: "Yoda", cip: "yoda9999" })}>Yoda</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(TopNav));
